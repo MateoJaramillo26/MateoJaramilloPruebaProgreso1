bf9f6a1123cc65ce0793f497882c4dc58a894b58
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-04-24 10:50:35 -0500

    Creacion de la base de datos con el comando add-migration y actualizacion de la misma con update-database

---
+++ Migrations/20240424154930_MateoJaramilloDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MJPruebaProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MateoJaramilloDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MJ_Tacos",
+                columns: table => new
+                {
+                    TacoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MJ_NombreTaco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MJ_DescripcionDelTaco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MJ_PrecioTaco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MJ_Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MJ_Tacos", x => x.TacoID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MJ_Tacos");
+        }
+    }
+}
