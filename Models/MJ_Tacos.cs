c58f9b863aa3b9808ecb742ff5e4e4f82927091f
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-04-24 10:53:24 -0500

    Borro de base de datos, controlador, y vistas. Se agrega el booleano Guacamole

--- Models/MJ_Tacos.cs
+++ Models/MJ_Tacos.cs
@@ -9,14 +9,16 @@ namespace MJPruebaProgreso1.Models
 
 
         [Required(ErrorMessage ="Debe ingresar el nombre para su taco")]
-        public string MJ_NombreTaco { get; set; }
+        public string? MJ_NombreTaco { get; set; }
         [Required(ErrorMessage ="Debe ingresar una descripcion para el producto")]
         [MinLength(10, ErrorMessage ="La descripcion del producto debe ser de almenos 10 caracteres")]
-        public string MJ_DescripcionDelTaco { get; set; }
+        public string? MJ_DescripcionDelTaco { get; set; }
         [Required(ErrorMessage ="El taco debe tener un precio")]
         [Range(1,25, ErrorMessage ="El taco debe tener un precio entre $1 y $25")]
         public decimal MJ_PrecioTaco { get; set; }
 
         public DateTime MJ_Fecha { get; set; }
+
+        public bool MJ_Guacamole { get; set; }
     }
 }
