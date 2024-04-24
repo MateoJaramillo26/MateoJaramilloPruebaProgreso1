84e7015a7cb31351e83adeb1a36d4f4297c6346b
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-04-24 10:42:56 -0500

    Se crea el modelo Taco por Mateo Jaramillo. Se crean los atributos y las validaciones con Data Annotations

---
+++ Models/MJ_Tacos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MJPruebaProgreso1.Models
+{
+    public class MJ_Tacos
+    {
+        public int TacoID { get; set; }
+        [Required(ErrorMessage ="Debe ingresar el nombre para su taco")]
+        public string MJ_NombreTaco { get; set; }
+        [Required(ErrorMessage ="Debe ingresar una descripcion para el producto")]
+        [MinLength(10, ErrorMessage ="La descripcion del producto debe ser de almenos 10 caracteres")]
+        public string MJ_DescripcionDelTaco { get; set; }
+        [Required(ErrorMessage ="El taco debe tener un precio")]
+        [Range(1,25, ErrorMessage ="El taco debe tener un precio entre $1 y $25")]
+        public decimal MJ_PrecioTaco { get; set; }
+
+        public DateTime MJ_Fecha { get; set; }
+    }
+}
